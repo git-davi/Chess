cf528b6f5ead1ba4ba00e687bd5c3e4aaa8d2938
git-davi <davidecasalini2@gmail.com>
2020-04-18 00:04:42 +0200

    async state change error fixed

--- chess-react/src/game/SearchGame.js
+++ chess-react/src/game/SearchGame.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect, useRef } from 'react';
 
 import {AuthContext} from '../App';
 import {axiosAuthWrapper as axiosAW} from './util/axiosAuthWrapper';
@@ -11,6 +11,17 @@ export default function SearchGame({ games, setGames }) {
     const [result, setResult] = useState({});
     const [searchState, setSearchState] = useState(false);
 
+    // this is for error on async jobs which could change
+    // state after the component unmounts, i need useRef 
+    // cause will produce a value that will stay alive for
+    // all the lifetime of this component (not only mount or unmount)
+    const mounted = useRef(false);
+    useEffect(() => {
+        mounted.current = true;
+
+        return () => mounted.current = false;
+    }, []);
+
 
     function startSearch() {
         setSearchState(true);
@@ -19,12 +30,13 @@ export default function SearchGame({ games, setGames }) {
             url: '/game/matchmaking/start',
         }, authContext)
         .then((res) => {
+            if (!mounted.current) return;
             setResult({status: 'success', message: 'Game found :'+res.data.game_uuid});
             setSearchState(false);
             setGames([...games, res.data.game_uuid]);
         })
         .catch((err) => {
-            console.log(err.response)
+            if(!mounted.current) return;
             setResult({status: 'failed', message: 'Failed to search game'});
             setSearchState(false);
         });
@@ -37,10 +49,14 @@ export default function SearchGame({ games, setGames }) {
             url: '/game/matchmaking/stop',
         }, authContext)
         .then((res) => {
+            if (!mounted.current) return;
             setResult({status: 'success', message: 'Matchmaking stopped with success'});
             setSearchState(false);
         })
-        .catch((err) => setResult({status: 'failed', message: 'We couldn\'t stop matchmaking'}))
+        .catch((err) => {
+            if (!mounted.current) return;
+            setResult({status: 'failed', message: 'We couldn\'t stop matchmaking'});
+        })
     }
 
 
@@ -50,16 +66,16 @@ export default function SearchGame({ games, setGames }) {
                 { searchState ?
                     <button className="btn btn-lg btn-danger" type="button" onClick={() => stopSearch()}>
                         Stop Search
-                        <svg class="bi bi-exclamation-octagon-fill ml-3" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd" d="M11.46.146A.5.5 0 0011.107 0H4.893a.5.5 0 00-.353.146L.146 4.54A.5.5 0 000 4.893v6.214a.5.5 0 00.146.353l4.394 4.394a.5.5 0 00.353.146h6.214a.5.5 0 00.353-.146l4.394-4.394a.5.5 0 00.146-.353V4.893a.5.5 0 00-.146-.353L11.46.146zM8 4a.905.905 0 00-.9.995l.35 3.507a.552.552 0 001.1 0l.35-3.507A.905.905 0 008 4zm.002 6a1 1 0 100 2 1 1 0 000-2z" clip-rule="evenodd"/>
+                        <svg className="bi bi-exclamation-octagon-fill ml-3" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fillRule="evenodd" d="M11.46.146A.5.5 0 0011.107 0H4.893a.5.5 0 00-.353.146L.146 4.54A.5.5 0 000 4.893v6.214a.5.5 0 00.146.353l4.394 4.394a.5.5 0 00.353.146h6.214a.5.5 0 00.353-.146l4.394-4.394a.5.5 0 00.146-.353V4.893a.5.5 0 00-.146-.353L11.46.146zM8 4a.905.905 0 00-.9.995l.35 3.507a.552.552 0 001.1 0l.35-3.507A.905.905 0 008 4zm.002 6a1 1 0 100 2 1 1 0 000-2z" clipRule="evenodd"/>
                         </svg>
                     </button>
                     :
                     <button className="btn btn-lg btn-success" type="button" onClick={() => startSearch()}>
                         Search a New Game
-                        <svg class="bi bi-search ml-3" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 011.415 0l3.85 3.85a1 1 0 01-1.414 1.415l-3.85-3.85a1 1 0 010-1.415z" clip-rule="evenodd"/>
-                            <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 100-11 5.5 5.5 0 000 11zM13 6.5a6.5 6.5 0 11-13 0 6.5 6.5 0 0113 0z" clip-rule="evenodd"/>
+                        <svg className="bi bi-search ml-3" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fillRule="evenodd" d="M10.442 10.442a1 1 0 011.415 0l3.85 3.85a1 1 0 01-1.414 1.415l-3.85-3.85a1 1 0 010-1.415z" clipRule="evenodd"/>
+                            <path fillRule="evenodd" d="M6.5 12a5.5 5.5 0 100-11 5.5 5.5 0 000 11zM13 6.5a6.5 6.5 0 11-13 0 6.5 6.5 0 0113 0z" clipRule="evenodd"/>
                         </svg>
                     </button>
                 }
