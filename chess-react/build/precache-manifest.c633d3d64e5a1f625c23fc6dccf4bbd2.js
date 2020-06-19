88c4765d614a10db66c8a0aabced4ffafea468db
git-davi <davidecasalini2@gmail.com>
2020-06-19 13:11:02 +0200

    diocan

---
+++ chess-react/build/precache-manifest.c633d3d64e5a1f625c23fc6dccf4bbd2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7051e75996ba29ea7628a9791394bbdb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5ae1bb3241f787fe9f8",
+    "url": "/static/js/2.b16acdac.chunk.js"
+  },
+  {
+    "revision": "2ae16cc244dcf3f4e0c5b73a9229d937",
+    "url": "/static/js/2.b16acdac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1535e431465de66aef1f",
+    "url": "/static/js/main.fdc1ef4d.chunk.js"
+  },
+  {
+    "revision": "0404b048bf1093c6cd45",
+    "url": "/static/js/runtime-main.643a7747.js"
+  }
+]);
