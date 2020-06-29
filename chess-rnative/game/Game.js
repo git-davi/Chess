dc5796ed94c67f1f9d7ba87697aef335310c1b7f
git-davi <davidecasalini2@gmail.com>
2020-06-29 17:31:29 +0200

    niente oh

--- chess-rnative/game/Game.js
+++ chess-rnative/game/Game.js
@@ -6,6 +6,9 @@ import Home from './Home';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+import { StyleSheet } from 'react-native';
+import { View } from 'native-base';
+
 
 const Stack = createStackNavigator();
 export const RefreshContext = React.createContext();
@@ -15,16 +18,25 @@ export default function Game() {
     const [refresh, setRefresh] = useState();
     
    return (
-        <RefreshContext.Provider value={{
-            refresh: refresh,
-            setRefresh: setRefresh
-        }}>
-            <NavigationContainer>
-                <Stack.Navigator initialRouteName="home">
-                    <Stack.Screen name="room" component={Room}/>
-                    <Stack.Screen name="home" component={Home}/>
-                </Stack.Navigator>
-            </NavigationContainer>
-        </RefreshContext.Provider>    
+        <View style={styles.container}>
+            <RefreshContext.Provider value={{
+                refresh: refresh,
+                setRefresh: setRefresh
+            }}>
+                <NavigationContainer>
+                    <Stack.Navigator initialRouteName="home">
+                        <Stack.Screen name="room" component={Room}/>
+                        <Stack.Screen name="home" component={Home}/>
+                    </Stack.Navigator>
+                </NavigationContainer>
+            </RefreshContext.Provider>
+        </View>
     );
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+});
